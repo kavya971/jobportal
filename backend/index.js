1c4ffe4df6feffc4f44bbe07bff804e5489f2c0c
kavya971 <22EG107B32@ANURAG.EDU.IN>
2025-05-20 12:30:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 
-  "http://localhost:5173"
-  "https://jobportal-qmgq.vercel.app/"
+  origin:"https://jobportal-qmgq.vercel.app"
   credentials: true,
 };
 app.use(cors(corsOptions));
